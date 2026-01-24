4e7084afb826905e6ffbe7bf10bfb9e5202c1a01
Alexey Nikitin <a.nikitin@aeza.net>
2026-01-24 18:35:26 +0300

    update

--- cmd/agent/main_test.go
+++ cmd/agent/main_test.go
@@ -7,12 +7,12 @@ import (
 )
 
 func TestAgent(t *testing.T) {
-	assert.True(t, true, "true should be true")
-	assert.False(t, false, "false should be false")
+	assert.True(t, true)
+	assert.False(t, false)
 }
 
 func TestReportInterval(t *testing.T) {
-	assert.Equal(t, 0, 5%5, "Should send when count=5")
-	assert.Equal(t, 0, 10%5, "Should send when count=10")
-	assert.NotEqual(t, 0, 6%5, "Should not send when count=6")
+	assert.Equal(t, 0, 5%5)
+	assert.Equal(t, 0, 10%5)
+	assert.Equal(t, 1, 6%5)
 }
--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -2,20 +2,16 @@ package main
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
-func TestUpdateHandler(t *testing.T) {
-	assert.True(t, 1 == 1, "1 should equal 1")
-	require.Equal(t, 2, 2, "2 should equal 2")
-}
+func TestServer(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
 
-func TestCounterLogic(t *testing.T) {
-	count := 0
-	count++
-
-	assert.Equal(t, 1, count, "Count should be 1")
-	assert.NotEqual(t, 0, count, "Count should not be 0")
+	t.Run("simple", func(t *testing.T) {
+		if 2+2 != 4 {
+			t.Fail()
+		}
+	})
 }
