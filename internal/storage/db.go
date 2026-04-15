0a72193f00a4e9f49c9ee6370da01ebb57d496d8
Alexey Nikitin <a.nikitin@aeza.net>
2026-04-15 07:49:32 +0300

    iter11

--- internal/storage/db.go
+++ internal/storage/db.go
@@ -119,6 +119,9 @@ func (s *DBStorage) GetAll() (map[string]float64, map[string]int64, error) {
 		}
 		gauges[name] = value
 	}
+	if err = rows.Err(); err != nil {
+		return nil, nil, err
+	}
 
 	counters := make(map[string]int64)
 	rows, err = s.db.Query("SELECT name, value FROM counters")
@@ -134,6 +137,9 @@ func (s *DBStorage) GetAll() (map[string]float64, map[string]int64, error) {
 			return nil, nil, err
 		}
 		counters[name] = value
+	}
+	if err = rows.Err(); err != nil {
+		return nil, nil, err
 	}
 
 	return gauges, counters, nil
