ca9389fb0c97c5ba4d992787f01fadd2f93e4cb6
Alexey Nikitin <a.nikitin@aeza.net>
2026-04-21 00:02:32 +0300

    iter14

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -22,7 +22,8 @@ func (r *responseRecorder) WriteHeader(statusCode int) {
 }
 
 func (r *responseRecorder) Write(b []byte) (int, error) {
-	return r.body.Write(b)
+	r.body.Write(b)
+	return r.ResponseWriter.Write(b)
 }
 
 func LoggingMiddleware(logger zerolog.Logger) func(next http.Handler) http.Handler {
@@ -78,6 +79,10 @@ func SignatureMiddleware(key string) func(next http.Handler) http.Handler {
 			if rec.body.Len() > 0 {
 				sign := signature.CalculateHash(rec.body.Bytes(), key)
 				w.Header().Set("HashSHA256", sign)
+			}
+
+			w.WriteHeader(rec.code)
+			if rec.body.Len() > 0 {
 				w.Write(rec.body.Bytes())
 			}
 		})
